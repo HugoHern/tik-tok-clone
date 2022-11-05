bb6052d8c7236befa0e1c27f7a5069cb6d3fafe2
HugoHern <hugo8954@gmail.com>
2022-11-05 18:01:14 -0400

    added starting components with basic css styling

--- src/App.js
+++ src/App.js
@@ -1,24 +1,19 @@
 
 import './App.css';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from './pages/Home'
+import Upload from './pages/Upload';
+import Header from './components/Header';
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+    <Header/>
+      <Routes>
+        <Route path='/' element={<Home/>}/>
+        <Route path='/upload' element={<Upload/>}/>
+      </Routes>
+    </BrowserRouter>
   );
 }
 
